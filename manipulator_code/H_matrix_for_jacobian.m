edb50b06f7163e7a433a8e1f6abbce1d6a762ba8
HyounjunOh <97611087+Hyounjun-Oh@users.noreply.github.com>
2023-01-28 07:12:51 +0900

    매니풀레이션 코드 푸쉬
    푸쉬

--- manipulator_code/H_matrix_for_jacobian.m
+++ manipulator_code/H_matrix_for_jacobian.m
@@ -2,18 +2,15 @@
 %% DRoC Lab
 function array = H_matrix_for_jacobian(theta_array)
 %% DH파라미터 기입
-d = [300 194 449.5 -190 350 183 228];
-a = [0 0 0 0 0 0 0];
-al = [90.00021 -90.0002 90.00021 -90.0002 90.00021 -90.0002 90.00021].*(pi/180);
-q = [theta_array(1) theta_array(2) theta_array(3) theta_array(4) theta_array(5) theta_array(6) theta_array(7)];
+d = [55 0 0 0];
+a = [0 200 200 100];
+al = [90 0 0 0].*(pi/180);
+q = [theta_array(1) theta_array(2) theta_array(3) theta_array(4)];
 %% 동차변환
 T01 = DHmatrix(q(1),d(1),a(1),al(1));
 T12 = DHmatrix(q(2),d(2),a(2),al(2));
 T23 = DHmatrix(q(3),d(3),a(3),al(3));
 T34 = DHmatrix(q(4),d(4),a(4),al(4));
-T45 = DHmatrix(q(5),d(5),a(5),al(5));
-T56 = DHmatrix(q(6),d(6),a(6),al(6));
-T67 = DHmatrix(q(7),d(7),a(7),al(7));
 
-array = [T01, T12, T23, T34, T45, T56, T67];
+array = [T01, T12, T23, T34];
 end
--- manipulator_code/ROBOMECH_4_DOF_MANIPULATOR.m
+++ manipulator_code/ROBOMECH_4_DOF_MANIPULATOR.m
@@ -5,6 +5,8 @@
 % 1. ROS 메세지로 Position과 Orientation값을 전달 받는다.
 % 2. 자코비안 역기구학을 이용하여 샘플링 타임동안의 4개의 조인트 값을 저장한다.
 % 3. 1 ~ 2번 까지의 과정이 시뮬레이션으로 잘 가동될 시 파이썬코드로 재구성하여 사용한다.
+%% 문제점
+% 초반 8샘플 타임에서 조인트 값 튐 발생. 원인 찾기.
 %%
 %close all
 clf
@@ -35,20 +37,17 @@ af = [0 0 0 0 0 0];
 ti = 0; tf = 2; t = linspace(ti,tf,100*(tf-ti));
 
 % Desired Pose
-desired_pose_1 = [-400 400 500 0 0 0];
+desired_pose_1 = [200 200 -200 0 0 0];
 
-desired_pose_array = [500 500 500 0 0 0;
-                      500 -500 0 0 0 0;
-                      -500 500 500 0 0 0
-                      0 0 1325 0 0 0];
+desired_pose_array = [100 200 200 0 0 0];
 % Current Pose
 current_pose = zeros(3,length(t));  %현재는 position만 고려한다.
 
 % Current Joints
-current_joints = zeros(7,length(t));
+current_joints = zeros(4,length(t));
 %% FIND CURRENT INITIAL POSE
 
-initial_joints = [-90.0002 1 1 1 1 1 90.00021].*(pi/180);
+initial_joints = [90.0001 0 0 0].*(pi/180);
 initial_pose = H_matrix(initial_joints);
 %% FIND TRAJECTORIES
 
@@ -59,7 +58,7 @@ M = [1 ti ti^2 ti^3 ti^4 ti^5;
      1 tf tf^2 tf^3 tf^4 tf^5;
      0 1 2*tf 3*(tf^2) 4*(tf^3) 5*(tf^4);
      0 0 2 6*tf 12*(tf^2) 20*(tf^3)];
-b = [initial_pose(1:3,8)', 0, 0, 0;vi;ai;desired_pose_array(1,:);vf;af];
+b = [initial_pose(1:3,5)', 0, 0, 0;vi;ai;desired_pose_array(1,:);vf;af];
 a = M\b;
 c = ones(size(t));
 
@@ -87,89 +86,89 @@ while 1
             for k = 1:length(t)-1
                 if k == 1
                     jacobian = jacobian7(initial_joints);
-                    [s,v,d] = svd(jacobian);
                     q_dot_f = pinv(jacobian) * ([vxd(k) vyd(k) vzd(k) 0 0 0])';
-                    current_joints(1:7,k) = initial_joints;
+                    current_joints(1:4,k) = initial_joints;
                     qf = initial_joints' + q_dot_f.*(t(k+1)-t(k));
                     Plot_Points = H_matrix(qf);
-                    current_pose(1:3,k) = Plot_Points(1:3,8);
-                    current_joints(1:7,k+1) = qf;
+                    current_pose(1:3,k) = Plot_Points(1:3,5);
+                    current_joints(1:4,k+1) = qf;
                     plot3(Plot_Points(1,:),Plot_Points(2,:),Plot_Points(3,:),'-or','LineWidth',3);
-                    axis([-1000,1000,-1000,1000,-500,2000]);
+                    axis([-500,500,-500,500,-100,500]);
                     grid on
                     pause(0.001)
                 else
                     Plot_Points = H_matrix(qf);
                     vf = [vxd(k);vyd(k);vzd(k);0;0;0];
-                    p_dot_ref = vf + (1.8*([pxd(k);pyd(k);pzd(k);0;0;0] - [Plot_Points(1:3,8);0;0;0]))./(t(k+1)-t(k));
-                    q_dot_f = pinv(jacobian7(qf)) * p_dot_ref;
+                    p_dot_ref = vf + (1.5*([pxd(k);pyd(k);pzd(k);0;0;0] - [Plot_Points(1:3,5);0;0;0]))./(t(k+1)-t(k));
+                    jacobian = jacobian7(qf);
+                    q_dot_f = pinv(jacobian) * p_dot_ref;
+                    
                     qf = qf + q_dot_f*(t(k+1)-t(k));
                     Plot_Points = H_matrix(qf);
-                    current_pose(1:3,k) = Plot_Points(1:3,8);
-                    current_joints(1:7,k+1) = qf;
+                    current_pose(1:3,k) = Plot_Points(1:3,5);
+                    current_joints(1:4,k+1) = qf;
                     plot3(Plot_Points(1,:),Plot_Points(2,:),Plot_Points(3,:),'-or','LineWidth',3);
-                    axis([-1000,1000,-1000,1000,-500,2000]);
+                    axis([-500,500,-500,500,-100,500]);
                     grid on
                     pause(0.001)
                 end
             end
         else
-%             initial_pose = H_matrix(qf);
-%             %% FIND TRAJECTORIES
-%             vf = [0 0 0 0 0 0];
-%             % Quintic Polynomial
-%             M = [1 ti ti^2 ti^3 ti^4 ti^5;
-%                  0 1 2*ti 3*(ti^2) 4*(ti^3) 5*(ti^4);
-%                  0 0 2 6*ti 12*(ti^2) 20*(ti^3);
-%                  1 tf tf^2 tf^3 tf^4 tf^5;
-%                  0 1 2*tf 3*(tf^2) 4*(tf^3) 5*(tf^4);
-%                  0 0 2 6*tf 12*(tf^2) 20*(tf^3)];
-%             b = [initial_pose(1:3,8)', 0, 0, 0;vi;ai;desired_pose_array(iter,:);vf;af];
-%             a = M\b;
-%             c = ones(size(t));
-%             
-%             % Desired trajectory
-%             pxd = a(1).*c + a(2).*t +a(3).*t.^2 + a(4).*t.^3 +a(5).*t.^4 + a(6).*t.^5;
-%             vxd = a(2).*c +2*a(3).*t +3*a(4).*t.^2 +4*a(5).*t.^3 +5*a(6).*t.^4;
-%             axd = 2*a(3).*c + 6*a(4).*t +12*a(5).*t.^2 +20*a(6).*t.^3;
-%             
-%             pyd = a(1,2).*c + a(2,2).*t +a(3,2).*t.^2 + a(4,2).*t.^3 +a(5,2).*t.^4 + a(6,2).*t.^5;
-%             vyd = a(2,2).*c +2*a(3,2).*t +3*a(4,2).*t.^2 +4*a(5,2).*t.^3 +5*a(6,2).*t.^4;
-%             ayd = 2*a(3,2).*c + 6*a(4,2).*t +12*a(5,2).*t.^2 +20*a(6,2).*t.^3;
-%             
-%             pzd = a(1,3).*c + a(2,3).*t +a(3,3).*t.^2 + a(4,3).*t.^3 +a(5,3).*t.^4 + a(6,3).*t.^5;
-%             vzd = a(2,3).*c +2*a(3,3).*t +3*a(4,3).*t.^2 +4*a(5,3).*t.^3 +5*a(6,3).*t.^4;
-%             azd = 2*a(3,3).*c + 6*a(4,3).*t +12*a(5,3).*t.^2 +20*a(6,3).*t.^3;
-%     
-%             for k = 1:length(t)-1
-%                 if k == 1
-%                     jacobian = jacobian7(qf);
-%                     [s,v,d] = svd(jacobian);
-%                     q_dot_f = pinv(jacobian) * ([vxd(k) vyd(k) vzd(k) 0 0 0])';
-%                     current_joints(1:7,k) = qf;
-%                     qf = qf + q_dot_f.*(t(k+1)-t(k));
-%                     Plot_Points = H_matrix(qf);
-%                     current_pose(1:3,k) = Plot_Points(1:3,8);
-%                     current_joints(1:7,k+1) = qf;
-%                     plot3(Plot_Points(1,:),Plot_Points(2,:),Plot_Points(3,:),'-or','LineWidth',3);
-%                     axis([-1000,1000,-1000,1000,-500,2000]);
-%                     grid on
-%                     pause(0.001)
-%                 else
-%                     Plot_Points = H_matrix(qf);
-%                     vf = [vxd(k);vyd(k);vzd(k);0;0;0];
-%                     p_dot_ref = vf + (1.8*([pxd(k);pyd(k);pzd(k);0;0;0] - [Plot_Points(1:3,8);0;0;0]))./(t(k+1)-t(k));
-%                     q_dot_f = pinv(jacobian7(qf)) * p_dot_ref;
-%                     qf = qf + q_dot_f*(t(k+1)-t(k));
-%                     Plot_Points = H_matrix(qf);
-%                     current_pose(1:3,k) = Plot_Points(1:3,8);
-%                     current_joints(1:7,k+1) = qf;
-%                     plot3(Plot_Points(1,:),Plot_Points(2,:),Plot_Points(3,:),'-or','LineWidth',3);
-%                     axis([-1000,1000,-1000,1000,-500,2000]);
-%                     grid on
-%                     pause(0.001)
-%                 end
-%             end
+            initial_pose = H_matrix(qf);
+            %% FIND TRAJECTORIES
+            vf = [0 0 0 0 0 0];
+            % Quintic Polynomial
+            M = [1 ti ti^2 ti^3 ti^4 ti^5;
+                 0 1 2*ti 3*(ti^2) 4*(ti^3) 5*(ti^4);
+                 0 0 2 6*ti 12*(ti^2) 20*(ti^3);
+                 1 tf tf^2 tf^3 tf^4 tf^5;
+                 0 1 2*tf 3*(tf^2) 4*(tf^3) 5*(tf^4);
+                 0 0 2 6*tf 12*(tf^2) 20*(tf^3)];
+            b = [initial_pose(1:3,5)', 0, 0, 0;vi;ai;desired_pose_array(iter,:);vf;af];
+            a = M\b;
+            c = ones(size(t));
+            
+            % Desired trajectory
+            pxd = a(1).*c + a(2).*t +a(3).*t.^2 + a(4).*t.^3 +a(5).*t.^4 + a(6).*t.^5;
+            vxd = a(2).*c +2*a(3).*t +3*a(4).*t.^2 +4*a(5).*t.^3 +5*a(6).*t.^4;
+            axd = 2*a(3).*c + 6*a(4).*t +12*a(5).*t.^2 +20*a(6).*t.^3;
+            
+            pyd = a(1,2).*c + a(2,2).*t +a(3,2).*t.^2 + a(4,2).*t.^3 +a(5,2).*t.^4 + a(6,2).*t.^5;
+            vyd = a(2,2).*c +2*a(3,2).*t +3*a(4,2).*t.^2 +4*a(5,2).*t.^3 +5*a(6,2).*t.^4;
+            ayd = 2*a(3,2).*c + 6*a(4,2).*t +12*a(5,2).*t.^2 +20*a(6,2).*t.^3;
+            
+            pzd = a(1,3).*c + a(2,3).*t +a(3,3).*t.^2 + a(4,3).*t.^3 +a(5,3).*t.^4 + a(6,3).*t.^5;
+            vzd = a(2,3).*c +2*a(3,3).*t +3*a(4,3).*t.^2 +4*a(5,3).*t.^3 +5*a(6,3).*t.^4;
+            azd = 2*a(3,3).*c + 6*a(4,3).*t +12*a(5,3).*t.^2 +20*a(6,3).*t.^3;
+    
+            for k = 1:length(t)-1
+                if k == 1
+                    jacobian = jacobian7(initial_joints);
+                    q_dot_f = pinv(jacobian) * ([vxd(k) vyd(k) vzd(k) 0 0 0])';
+                    current_joints(1:4,k) = initial_joints;
+                    qf = initial_joints' + q_dot_f.*(t(k+1)-t(k));
+                    Plot_Points = H_matrix(qf);
+                    current_pose(1:3,k) = Plot_Points(1:3,5);
+                    current_joints(1:4,k+1) = qf;
+                    plot3(Plot_Points(1,:),Plot_Points(2,:),Plot_Points(3,:),'-or','LineWidth',3);
+                    axis([-500,500,-500,500,-100,500]);
+                    grid on
+                    pause(0.001)
+                else
+                    Plot_Points = H_matrix(qf);
+                    vf = [vxd(k);vyd(k);vzd(k);0;0;0];
+                    p_dot_ref = vf + (1*([pxd(k);pyd(k);pzd(k);0;0;0] - [Plot_Points(1:3,5);0;0;0]))./(t(k+1)-t(k));
+                    q_dot_f = pinv(jacobian7(qf)) * p_dot_ref;
+                    qf = qf + q_dot_f*(t(k+1)-t(k));
+                    Plot_Points = H_matrix(qf);
+                    current_pose(1:3,k) = Plot_Points(1:3,5);
+                    current_joints(1:4,k+1) = qf;
+                    plot3(Plot_Points(1,:),Plot_Points(2,:),Plot_Points(3,:),'-or','LineWidth',3);
+                    axis([-500,500,-500,500,-100,500]);
+                    grid on
+                    pause(0.001)
+                end
+            end
         end
     else
         disp('종료')
