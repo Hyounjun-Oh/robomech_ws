c718601dbf479c7a00198a6fe94b6231d499e330
HyounjunOh <97611087+Hyounjun-Oh@users.noreply.github.com>
2023-02-03 02:25:54 +0900

    최대속도 계산 완료
    Matlab 주석에 기재되어있음.

--- inverse_kinematics/maximum_velocity_calculation.m
+++ inverse_kinematics/maximum_velocity_calculation.m
@@ -1,5 +1,20 @@
+%Result
+% | deegre | Vx | Vy | Rz |
+% [0    0.6     0       0
+%  45   0.3     0.3     0
+%  90   0       0.6     0
+%  135  0.3     -0.3    0
+%  180  -0.6    0       0
+%  225  -0.3    -0.3    0
+%  275  0       -0.6    0
+%  315  -0.3     0.3    0]
+
+% Max Vx, Vy = 0.3 m/s
+% Max diagonal direction 0.6 m/s
+% Max Rotational direction 1.7647 rad/s
+%%
 %rad/s
-w = [0 -16 -16 0];
+w = [12 -12 12 -12];
 
 % The data of real robot
 % a = 0.395;
@@ -12,9 +27,6 @@ b = 0.170;
 R = 0.050;
 
 radius_sum = a+b;
-
-
-
 
 % result = [Vx, Vy, w(z-axis)]
 result = pinv([1 -1 -radius_sum; ...
